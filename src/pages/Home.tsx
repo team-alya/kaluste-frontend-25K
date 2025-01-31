3d959c51ee06b6b2f16f53fb54dbd52a273ce7aa
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2025-01-31 13:55:21 +0200

    some code

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,25 @@
+const Home = () => {
+
+    return(
+        
+        <div>
+            <div>
+                <p>Valitse toiminta</p>
+            </div>
+            <div>
+                <h1>Hei!</h1>
+                <p>Tervetuloa töihin, mitä haluaisit tehdä?</p>
+
+            </div>
+            <div>
+                <p>"Tunnista tuote" -nappi tähän</p>
+            </div>
 
 
-const home = () => {
-
-    
-
-    return (
-        <>
-        </>
+        </div>
+ 
     )
+
 }
+
+export default Home;
