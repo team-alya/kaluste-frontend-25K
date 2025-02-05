f8e8d8df4953614a693a284d7332f50bfe754560
Viivi Salin <salin.viivi@gmail.com>
2025-02-05 09:29:59 +0200

    Unused code removed
    Old camera function and unused menu button removed

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -26,11 +26,6 @@ const Navbar = () => {
             </div>
             <div className="flex items-center flex-shrink-0 text-white mr-6">
             </div>
-            <div className="block lg:hidden">
-                <button className="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
-                <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
-                </button>
-            </div>
             <div>
                 <div>
                     <button className="inline-block text-sm px-2 py-2 leading-none border rounded-full text-white border-zinc-600 bg-zinc-600 hover:border-transparent hover:text-teal-500 hover:bg-black mt-4 lg:mt-0">
--- src/pages/Camera.tsx
+++ src/pages/Camera.tsx
@@ -1,10 +1,9 @@
 import { useRef, useState } from "react"
 import { Camera } from "react-camera-pro";
+import { Focus } from "lucide-react";
 
 
 const CameraApp: React.FC = () => {
-    //const videoRef = useRef<HTMLVideoElement | null>(null);
-    //const canvasRef = useRef<HTMLCanvasElement | null>(null);
     const [photo, setPhoto] = useState<string | null>(null);
     const cameraRef = useRef<any>(null);
 
@@ -12,57 +11,9 @@ const CameraApp: React.FC = () => {
         if (cameraRef.current) {
             const imageDataUrl = cameraRef.current.takePhoto();
             setPhoto(imageDataUrl);
-            console.log("Kuva otetty ja tallennettu")
+            console.log("Kuva otettu ja tallennettu")
         }
     };
-
-    /*
-    useEffect(() => {
-        navigator.mediaDevices
-            .getUserMedia({ video: { facingMode: "enviroment" } })
-            .then((stream) => {
-                if (videoRef.current) {
-                    videoRef.current.srcObject = stream;
-                }
-            })
-            .catch((err) => console.error("Kamera häiriö:", err));
-    }, []); 
-
-    const capturePhoto = () => {
-        const canvas = canvasRef.current;
-        const video = videoRef.current;
-        if (canvas && video) {
-            const context = canvas.getContext("2d");
-            if (context) {
-                canvas.width = video.videoWidth;
-                canvas.height = video.videoHeight;
-                context.drawImage(video, 0, 0, canvas.width, canvas.height);
-                const capturedPhoto = canvas.toDataURL("image/png");
-                setPhoto(capturedPhoto);
-                console.log("Kuva otettu ja tallennettu")
-            }
-        }
-    };
-
-    return (
-        <div className="flex flex-col items-center justify-center p-5 bg-gray-100 min-h-screen space-y-4">
-            <video
-                ref={videoRef}
-                autoPlay
-                playsInline
-                className="w-full max-w-lg rounded-lg shadow-md"
-            />
-            <button
-                onClick={capturePhoto}
-                className="mt-4 px-6 py-3 text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700 transition"
-            >
-                Ota kuva
-            </button>
-            <canvas ref={canvasRef} className="hidden"></canvas>
-        </div>
-    );
-
-    */
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen p-5">
@@ -90,9 +41,9 @@ const CameraApp: React.FC = () => {
             </div>
             <button
                 onClick={capturePhoto}
-                className="mt-4 px-6 py-3 text-white bg-green-600 rounded-lg shadow-md hover:bg-green-700 transition"
+                className="mt-4 px-6 py-3 text-white bg-green-600 rounded-full shadow-md hover:bg-green-700 transition"
             >
-                Ota kuva
+                <Focus color="#ffffff" strokeWidth={2.5} />
             </button>
         </div>
     );
