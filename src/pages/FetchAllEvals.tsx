6f1644f281537781c9c66a477eab784c0b9838e0
anastasialamberg <anastasia.lamberg@gmail.com>
2025-02-27 21:24:06 +0200

    Display image in the FetchAllEvals.tsx

--- src/pages/FetchAllEvals.tsx
+++ src/pages/FetchAllEvals.tsx
@@ -1,47 +1,62 @@
 import { useState } from "react";
 
-export default function FetchAllEvals() { 
+export default function FetchAllEvals() {
+  const [evals, setEvals] = useState([]);
+  const [isFetched, setIsFetched] = useState<boolean>(false);
 
-    const [evals, setEvals] = useState([]);
-    const [isFetched, setIsFetched] = useState<boolean>(false);
-
-    const fetchEvals = () => {
-
-        fetch('https://kalustearvio-25k-backend-kalustearvio-25k.2.rahtiapp.fi/api/image/all')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch evaluations');
-            }
-            return response.json();
-        })
-        .then(data => {
-            setEvals(data);
-            setIsFetched(true);
-        })
-        .catch(error => console.error(error));
-
-    }
-
-    return(
-        <div>
-            <h1>Fetch All Evaluations</h1>
-
-            <button onClick={fetchEvals}>Fetch</button>
-
-
-            {isFetched && ( 
-                <div>
-                    {evals.map((e: any) => (
-                        <div className="m-5" key={e.id}>
-                            <h2>{e.id}</h2>
-                            <p>{e.evaluation.brand}</p>
-                            <p>{e.evaluation.color}</p>
-                            <p>{e.evaluation.condition}</p>
-                </div>
-                    ))}
-                </div>
-            )}
-        </div>
+  const fetchEvals = () => {
+    fetch(
+      "https://kalustearvio-25k-backend-kalustearvio-25k.2.rahtiapp.fi/api/evaluation/all "
     )
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch evaluations");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setEvals(data);
+        setIsFetched(true);
+        console.log(data);
+      })
 
+      .catch((error) => console.error(error));
+  };
+
+  return (
+    <div>
+      <h1>Fetch All Evaluations</h1>
+
+      <button onClick={fetchEvals}>Fetch</button>
+
+      {isFetched && (
+        <div>
+          {evals.map((e: any) => (
+            <div className="m-5" key={e.id}>
+              <h2>{e.id}</h2>
+              <p>{e.evaluation.brand}</p>
+              <p>{e.evaluation.color}</p>
+              <p>{e.evaluation.condition}</p>
+
+             {/* Display image*/}
+             
+              <img
+                src={
+                  e.image.startsWith("data:image")
+                    ? e.image
+                    : `data:image/jpeg;base64,${e.image}`
+                }
+                alt="Evaluation"
+                style={{
+                  maxWidth: "300px",
+                  height: "auto",
+                  borderRadius: "10px",
+                }}
+              />
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
