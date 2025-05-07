13c0c4ccbd6afec8ba751c289c00c84a7953b9c4
arinasold <arina.soldan@gmail.com>
2025-05-07 09:41:18 +0300

    Conflict resolved

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import cam from "/assets/camera.png";
-import check from "/assets/check.png";
+import check from "/assets/check.png";  
 import thumb from "/assets/ok.png";
 import archive from "/assets/archive.png";
 import admin from "/assets/admin.png";
@@ -11,17 +11,15 @@ const Home = () => {
   const navigate = useNavigate();
   const location = useLocation();
   const username = window.localStorage.getItem("username") || null;
-  const role = window.localStorage.getItem("role");
+  const role = window.localStorage.getItem("role") || null;
 
   return (
 
       <div className="m-8">
       <div className="mb-6">
         <div className="flex items-center space-x-2">
-          <h1 className="text-4xl font-bold">Hei,</h1>
-          <p className="text-4xl font-bold text-emerald-700 text-primary">
-            {username}
-          </p>
+        <h1 className="text-4xl font-bold">Hei,</h1>
+        <p className="text-4xl font-bold text-emerald-700 text-primary">{username}</p>
         </div>
         <p>Tervetuloa töihin, mitä haluaisit tehdä?</p>
       </div>
@@ -55,8 +53,9 @@ const Home = () => {
       <div
         className="grid"
         style={{
-        gridTemplateColumns: "repeat(auto-fit, minmax(150px, max-content))",
-        gap: "16px",}}>
+        gridTemplateColumns: "repeat(auto-fit, minmax(150px, 150px))",
+        gap: "16px",
+        maxWidth: "550px"}}>
 
         {/* button that navigates to product list */}
         <button
@@ -102,29 +101,9 @@ const Home = () => {
         >
           <p className="mt-23 text-white">Arkisto</p>
         </button>
-    </div>
-    </div>
 
-        <div>
-          {/* button that navigates to archived list */}
-          <button
-            className=""
-            style={{
-              backgroundImage: `url(${archive})`,
-              height: "150px",
-              width: "150px",
-            }}
-            onClick={() => {
-              navigate("/archive", { state: { from: location.pathname } });
-            }}
-          >
-            <p className="mt-23 text-white">Arkisto</p>
-          </button>
-        </div>
-
-        <div>
-          {/* button that navigates to admin */}
-          {role === "admin" && (
+            {/* button that navigates to admin */}
+            `{role === "admin" && (
             <button
               className=""
               style={{
@@ -141,9 +120,29 @@ const Home = () => {
               <p className="mt-23 text-white">Admin</p>
             </button>
           )}
-        </div>
-      </div>
+
     </div>
+    </div>
+
+      ) : (
+        <div>
+          {/* button that navigates to camera page */}
+          <button
+            className=""
+            style={{
+              backgroundImage: `url(${cam})`,
+              height: "150px",
+              width: "150px",
+            }}
+            onClick={() => navigate("/camera", { state: { username, from: location.pathname } })}
+          >
+            <p className="mt-23 text-white">Tunnista tuote</p>
+        </button>
+      </div>
+    )}
+
+      </div>
+
   );
 };
 
